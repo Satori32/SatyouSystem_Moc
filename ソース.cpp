1fdcd2777cca0a844642c58bd95368edf1961019
Satori32 <Huga8989+githubf@gmail.com>
2024-11-08 13:50:42 +0900

    プロジェクト ファイルを追加します。

---
+++ ソース.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+//魔法でコールを試した後は実態を実装してスタンドアローンにするように！
+
+struct CallMilionArthur {
+	void* P = NULL;
+};
+
+struct CallFreeScenarioSystem {
+	void* P = NULL;
+};
+
+struct CallActiveTimeBattleSystem {
+	void* P = NULL;
+};
+
+struct CallProFileSystem {
+	void* P = NULL;
+};
+
+struct CallCameraSystem {
+	void* P = NULL;
+};
+
+struct CallGraphicSystem {
+	void* P = NULL;
+};
+
+struct CallSoundSystem {
+	void* P = NULL;
+};
+
+struct CallNetWarkSystem {
+	void* P = NULL;
+};
+
+struct CallEmotionalSystem {
+	void* P = NULL;
+};
+
+struct CallTakeSystem {
+	void* P = NULL;
+};
+
+struct LongUseBaseSystem {
+	CallMilionArthur CMA;
+	CallFreeScenarioSystem CF;
+	CallActiveTimeBattleSystem CAT;
+	CallProFileSystem CP;
+	CallCameraSystem CA;
+	CallEmotionalSystem CE;
+	CallTakeSystem CT;
+	CallGraphicSystem CG;
+	CallSoundSystem CS;
+	CallNetWarkSystem CN;
+};
